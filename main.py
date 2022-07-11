99d1ccadcd33ca882b2f886d40218cdff6d1894b
sovietor <94129991+heygdrg@users.noreply.github.com>
2022-07-11 09:08:01 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,12 +8,27 @@ from colorama import *
 
 System.Size(170, 47)
 
+def nitro():
+    caracteres =  ['a', 'b', 'c', 'd', 'e', 'F','g','h','i','j','k','l','m','n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x','y','z','0','1','2', '3','4', '5','6','7','8','9']
+
+    while True:
+        nitrocode = ""
+        for i in range(16):
+            nitrocode = f"{nitrocode}{random.choice(caracteres)}"
+        print(
+            Fore.RED + F"https://discord.gift/{nitrocode}")
+        time.sleep(0.025)
+        with open("nitros. txt", "a+") as nitroFile:
+            nitroFile.write(f"https://discord.gift/{nitrocode}\n")
+
+            nitroFile.close
+
 def gen():
     characters = string.ascii_lowercase + string.digits
     for i in range(99999999999999999999999999999):
-        # print(Fore.RED + "https://discord.gg/%s\n" .join(random.sample(characters, 32)))
         print(
             Fore.RED + ">[ ] https://discord.gift/%s  " % "".join(random.sample(characters, 16)))
+        with open('nitro.txt', 'a+') as file: file.write("df")
         time.sleep(0.025)
     input()
 
@@ -32,11 +47,9 @@ banner1 = """
 
 
 print(Colorate.Vertical(Colors.purple_to_blue, Center.XCenter(banner1 + '\n\n')))
-
-#Write.Print("[!] a lot of link will be generate but the only good one will be write in green there is aproximatively one gren for 10 000 red just wait for the green one !", Colors.blue_to_red, interval=0.001)
-#print()
 Write.Input("[?] enter to start generation", Colors.blue_to_red, interval=0.001)
 print()
 print()
 print()
-gen()
+
+nitro()
